89ac6e49d5c57c2e8d01b265afb3ce35d26d6f33
Kieu Tien Thanh <kieutienthanh207@gmail.com>
2024-12-27 11:36:41 +0700

    setup code base

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,12 @@
+const RootLayout = () => {
+  return (
+    <>
+      <div>A Root Layout</div>
+      {/* sidebar */}
+      {/* topbar */}
+      {/* maincontent */}
+    </>
+  )
+}
+
+export default RootLayout
---
+++ src/route.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+
+import RootLayout from "@/layouts/RootLayout";
+import Login from "@/pages/Login";
+import Signup from "@/pages/Signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/signup",
+    element: <Signup/>
+  }
+])
+
+export default router
